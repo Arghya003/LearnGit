0351fe79c4559cb6274e0143ffbe646cc81e8a40
Arghya Guha <arghyaguha105@gmail.com>
2022-04-20 12:27:42 +0530

    All files commited

--- add.java
+++ add.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class add{
     public static void main(String args []){
         int a=6,b=7;
-        System.out.println(a+b);
+        System.out.println("sum is"+a+b);
     }
 }
